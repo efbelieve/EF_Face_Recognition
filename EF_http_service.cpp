cb0522450531e4109c270e7550cef42b6f6b1145
linyufeng <2272233800@qq.com>
2017-10-05 02:33:51 -0700

    develop EF_http_service.cpp

--- EF_http_service.cpp
+++ EF_http_service.cpp
@@ -2,46 +2,64 @@
 #include <string.h>
 #include <sys/types.h>          
 #include <sys/socket.h>
+#include "UrlResolve.h"
 
 EF_http_service::EF_http_service()
 {
-	m_url_addr = "";
-	m_message  = "";
-
-}
-
-EF_http_service::~EF_http_service()
-{
-
-}
-
-bool EF_http_service::set_url(const string url_addr)
-{
-	m_url_addr = url_addr;
-}
-
-bool EF_http_service::http_post()
-{
-	if(m_url_addr == "")
-	{
-		printf("please set url!\n");
-		return FALSE;
-	}
-	
-	m_head  = "POST "+m_url_addr+" HTTP/1.1\r\n";
-	m_head += "Connection: Keep-Alive\r\n"
-
-	int socket_fd = 0;
-	struct sockaddr_in serv_addr;
-
+	socket_fd = -1;
 	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
 	if(socket_fd < 0)
 	{
-		printf("socket error!\n");
-		return FALSE;
+		cout << "socket error!" << endl;
+		return false;
 	}
+}
 
+EF_http_service::~EF_http_service()
+{
+	close(socket_fd);
+}
+
+bool EF_http_service::set_url(const string &url_addr)
+{
+	url_st url_temp;
+	UrlResolve urlresolve;
+	urlresolve.set_url(url_addr);
+	if(urlresolve.url_resolve(url_temp))
+	{
+		cout << "EF_http_service::set_url error!\n" << endl;
+		return false;
+	}
+	m_url_host = url_temp.HOST;
+	m_url_path = url_temp.PATH;
+
+	m_request_line = "POST " + m_url_path + " HTTP/1.1\r\n";
+
+	return true;
+}
+
+bool EF_http_service::add_request_head(string name, string value)
+{
+	m_request_head += name + ": " + value;
+	return true;
+}
+
+bool EF_http_service::http_post()
+{
+	if(m_url_host.empty())
+	{
+		cout << "please set url!" << endl;
+		return false;
+	}
+	
+	struct hostent * hostaddr;
+	hostaddr = gethostbyname(m_url_host);
+
+	struct sockaddr_in serv_addr;
 	memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+	serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("203.195.192.24");
+    serv_addr.sin_port = htons((short)80);
 
 }
--- EF_http_service.h
+++ EF_http_service.h
@@ -9,12 +9,16 @@ public:
 	EF_http_service();
 	~EF_http_service();
 	
-	bool set_url(const string url_addr);
+	bool set_url(const string &url_addr);
+	bool add_request_head(string name, string value);
 	bool http_post();
 private:
-	string m_url_addr;
+	int socket_fd;
 	string m_message;
-	string m_head;
+	string m_request_line;
+	string m_request_head;
+	string m_url_host;
+	string m_url_path;
 }
 
 
